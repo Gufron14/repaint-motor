1c1575cb4b9d43b904fa1162fd9a9169e6ed418a
Bagus Sonara <bagus.surend@gmail.com>
2025-05-12 21:15:03 +0700

    hakim

--- database/migrations/2025_02_20_225258_create_portfolios_table.php
+++ database/migrations/2025_02_20_225258_create_portfolios_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
-            $table->string('image');
+            // $table->string('image');
+            $table->string('type')->default('image');
+            $table->string('media');
             $table->timestamps();
         });
     }
