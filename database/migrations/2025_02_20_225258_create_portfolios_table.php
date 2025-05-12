47af7ef105d9dab54eea45c9855e03563a5e1336
Bagus Sonara <bagus.surend@gmail.com>
2025-05-12 22:14:37 +0700

    Revert "hakim"
    This reverts commit 1c1575cb4b9d43b904fa1162fd9a9169e6ed418a.

--- database/migrations/2025_02_20_225258_create_portfolios_table.php
+++ database/migrations/2025_02_20_225258_create_portfolios_table.php
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
-            // $table->string('image');
-            $table->string('type')->default('image');
-            $table->string('media');
+            $table->string('image');
             $table->timestamps();
         });
     }
