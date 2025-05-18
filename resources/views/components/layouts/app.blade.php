b16ac266e327751834b39315ea2c259e1d8c1b4a
Gufron <gupron.nurjalil14@gmail.com>
2025-05-18 17:02:19 +0700

    icon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,14 +8,19 @@
 
     <link rel="stylesheet" href="{{ asset('asset/css/style.css') }}">
 
+    <link rel="shortcut icon" href="{{ asset('asset/img/logo.png') }}">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+        rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <script type="text/javascript" src="{{ config('midtrans.snap_url') }}" data-client-key="{{ config('midtrans.client_key') }}"></script>
+    <script type="text/javascript" src="{{ config('midtrans.snap_url') }}"
+        data-client-key="{{ config('midtrans.client_key') }}"></script>
     @livewireStyles
 </head>
 
@@ -30,10 +35,10 @@
         </div>
     </div>
 
-        <!-- Floating WhatsApp Button -->
-        <a href="https://wa.me/62895372513547?text=Hallo%20Hype%20Custom%20Project" target="_blank" class="whatsapp-button">
-            <i class="bi bi-whatsapp"></i> Hubungi Kami
-        </a>
+    <!-- Floating WhatsApp Button -->
+    <a href="https://wa.me/62895372513547?text=Hallo%20Hype%20Custom%20Project" target="_blank" class="whatsapp-button">
+        <i class="bi bi-whatsapp"></i> Hubungi Kami
+    </a>
 
     <footer class="bg-body-tertiary text-center text-lg-start mt-5">
         <div class="d-flex justify-content-between align-items-middle mx-5 py-3">
@@ -44,7 +49,9 @@
             <div class="align-self-center">
                 <div class="fs-5">
                     <a href="https://wa.me/62895372513547"><i class="bi bi-telephone-fill"></i></a>
-                    <a href="https://www.instagram.com/hypecustom.project?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i class="bi bi-instagram"></i></a>
+                    <a
+                        href="https://www.instagram.com/hypecustom.project?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><i
+                            class="bi bi-instagram"></i></a>
                 </div>
                 © 2025 Hype Custom Project
             </div>
